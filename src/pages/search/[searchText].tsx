f6f529d368ad7bb5d544e8d5f8d6fb42ae99a78a
Aran Hayer <ashayer@users.noreply.github.com>
2023-01-19 00:00:42 -0800

    added loading state to search

--- src/pages/search/[searchText].tsx
+++ src/pages/search/[searchText].tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import SearchResultCard from "../../components/SearchResultCard";
 import placeHolder from "../../assets/albumPlaceholder.jpg";
+import loadingGif from "../../assets/loading.gif";
 
 const SearchResults: NextPage = () => {
   const [searchResults, setSearchResults] =
@@ -29,27 +30,34 @@ const SearchResults: NextPage = () => {
         const topAlbumResult = data.albums?.items[0];
         const topArtistResult = data.artists?.items[0];
 
-        searchText as string;
+        console.log(topAlbumResult?.name, topArtistResult?.name);
 
         if (
           topArtistResult?.name.toLowerCase().includes(searchText as string)
         ) {
           setTopResult(topArtistResult);
-        }
-
-        if (topAlbumResult?.name == (searchText as string)) {
+        } else if (
+          topArtistResult?.name.toLowerCase().includes(searchText as string)
+        ) {
           setTopResult(topAlbumResult);
         }
       },
     }
   );
 
-  return (
+  return searchQuery.isLoading ? (
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-36 w-36 animate-pulse">
+        <Image src={loadingGif} alt="" />
+      </div>
+    </div>
+  ) : (
     <div className="mx-auto flex max-w-7xl flex-col pb-10">
       <div className="text-center">
         <p className="p-2 text-xl font-bold">Top Result</p>
         {topResult?.type === "artist" ? (
           <button className="transition-all hover:scale-95">
+            <p>Artist</p>
             <Image
               className="aspect-square rounded-xl object-cover"
               title={topResult.name}
@@ -61,10 +69,20 @@ const SearchResults: NextPage = () => {
             <p className="text-lg font-bold">{topResult.name}</p>
           </button>
         ) : (
-          <>album</>
+          <button className="transition-all hover:scale-95">
+            <p>Album</p>
+            <Image
+              className="aspect-square rounded-xl object-cover"
+              title={topResult?.name}
+              src={topResult?.images[1]?.url || placeHolder}
+              alt="artist cover"
+              height={208}
+              width={208}
+            />
+            <p className="text-lg font-bold">{topResult?.name}</p>
+          </button>
         )}
       </div>
-
       <div>
         <p className="p-6 font-bold md:text-3xl">albums</p>
         <div className="grid grid-cols-3 gap-2 px-2 md:grid-cols-4 2xl:grid-cols-6">
